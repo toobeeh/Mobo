6d7f8574746a646cb6bdd43d6069e5158a09e71b
tobeh <tobiasch@gmx.at>
2020-08-18 02:26:30 +0200

    V1: Simple move command, warn and manual

---
+++ Mobo/Commands.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using DSharpPlus;
+using DSharpPlus.Interactivity;
+using System.Linq;
+using System.Runtime;
+
+namespace Mobo
+{
+    public class Commands : BaseCommandModule
+    {
+        [Command("manual")]
+        [Description("Show the manual for bot usage")]
+        [RequireUserPermissions(DSharpPlus.Permissions.Administrator)]
+        [RequireGuild()]
+        public async Task Manual(CommandContext context)
+        {
+            string msg = "**Hi!** \nI'm Mobo, your move bot - cute, huh? :3\nMy job is to move offtopic conversations into the right channel.\nTo do so, you will have to call following command:\n\n";
+            msg += "*mobo:move* `limit` `@role` `#channel`\nLimit sets how many messages you want to show in the notification.\nRole sets which users should have a cooldown - set to `@everyone`.\nChannel sets the channel where the conversation should continue.\n\nExample:\n`mobo:move 10 @everyone #main`\n\n";
+            msg += "\n\n Try *mobo:warn* too!";
+            await context.RespondAsync(msg);
+        }
+
+        [Command("move")]
+        [Description("Move a conversation into another channel")]
+        [RequireUserPermissions(DSharpPlus.Permissions.Administrator)]
+        [RequireGuild()]
+        public async Task Move(CommandContext context, int limit, DiscordRole tempBan, DiscordChannel target)
+        {
+
+            await context.Message.DeleteAsync();
+            var channel = context.Channel;
+            var lastMessages = await channel.GetMessagesAsync(limit);
+            await context.Channel.SendMessageAsync("This conversation belongs to " + target.Mention + " and was moved there! \n" + context.Channel.Mention + " is now closed for a minute to cool things down.\n\n*Remember to avoid offtopic chats!*");
+            await channel.AddOverwriteAsync(tempBan, DSharpPlus.Permissions.None, DSharpPlus.Permissions.SendMessages, "Temp timeout start");
+            
+            var embedChat = new DiscordEmbedBuilder
+            {
+                Title = "**Moved chat from *#" + context.Channel.Name + "* **",
+                Description = "This conversation didn't meet the channel topic and should be continued here.\n\n\n<a:typing:745068588909592737>"
+            };
+            embedChat.Color = new DiscordColor("#ff1744");
+            lastMessages.Reverse().ToList().ForEach(
+                (m) => {
+                    embedChat.AddField("**" + m.Author.Username + "**", m.Content);
+                }
+            );
+
+            await target.SendMessageAsync(embed: embedChat);
+            await Task.Delay(60000);
+            await channel.AddOverwriteAsync(tempBan, DSharpPlus.Permissions.SendMessages, DSharpPlus.Permissions.None, "Temp timeout stop");
+
+        }
+
+        [Command("warn")]
+        [Description("Send a soft warning if the chat tends to get out of hand.")]
+        [RequireGuild()]
+        public async Task Warn(CommandContext context)
+        {
+            await context.Message.DeleteAsync();
+            await context.Channel.SendMessageAsync("**Watch out!**\nMaybe another channel fits better?");
+
+        }
+    }
+}
---
+++ Mobo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext;
+using System.IO;
+
+namespace Mobo
+{
+    class Program
+    {
+        public static DiscordClient Client { get; private set; }
+        public static CommandsNextExtension Commands { get; private set; }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Awwww! You woke Mobo :o");
+
+            Client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NzQ1MDExNDQwOTkzMzA0NzQ2.Xzrj-Q.6G01qQcPYQKJsPJD-CIhzB0ugks",// File.ReadAllText("C:/Users/Tobi/Desktop/token.txt"),
+                TokenType = TokenType.Bot
+            });
+            Commands = Client.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { "mobo:" },
+                DmHelp = false,
+                IgnoreExtraArguments = true
+            });
+            Client.GuildCreated += onjoin;
+            Commands.RegisterCommands<Commands>();
+            await Client.ConnectAsync();
+
+            Console.WriteLine("Mobo is ready to move!!");
+
+            await Task.Delay(-1);
+        }
+
+        private static async Task onjoin(GuildCreateEventArgs e)
+        {
+            await e.Guild.GetDefaultChannel().SendMessageAsync("Heya!\nCheck out the command *mobo:manual* ");
+        }
+    }
+}
