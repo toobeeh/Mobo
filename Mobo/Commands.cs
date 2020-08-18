e326ecd2b0fd4bbe32c7dec247c4d3ba3f9174a7
tobeh <tobiasch@gmx.at>
2020-08-18 11:37:53 +0200

    Removed role parameter

--- Mobo/Commands.cs
+++ Mobo/Commands.cs
@@ -21,7 +21,7 @@ namespace Mobo
         public async Task Manual(CommandContext context)
         {
             string msg = "**Hi!** \nI'm Mobo, your move bot - cute, huh? :3\nMy job is to move offtopic conversations into the right channel.\nTo do so, you will have to call following command:\n\n";
-            msg += "*mobo:move* `limit` `@role` `#channel`\nLimit sets how many messages you want to show in the notification.\nRole sets which users should have a cooldown - set to `@everyone`.\nChannel sets the channel where the conversation should continue.\n\nExample:\n`mobo:move 10 @everyone #main`\n\n";
+            msg += "*mobo:move* `limit` `@role` `#channel`\nLimit sets how many messages you want to show in the notification.\nChannel sets the channel where the conversation should continue.\n\nExample:\n`mobo:move 10 #main`\n\n";
             msg += "\n\n Try *mobo:warn* too!";
             await context.RespondAsync(msg);
         }
@@ -30,14 +30,14 @@ namespace Mobo
         [Description("Move a conversation into another channel")]
         [RequireUserPermissions(DSharpPlus.Permissions.Administrator)]
         [RequireGuild()]
-        public async Task Move(CommandContext context, int limit, DiscordRole tempBan, DiscordChannel target)
+        public async Task Move(CommandContext context, int limit, DiscordChannel target)
         {
 
             await context.Message.DeleteAsync();
             var channel = context.Channel;
             var lastMessages = await channel.GetMessagesAsync(limit);
             await context.Channel.SendMessageAsync("This conversation belongs to " + target.Mention + " and was moved there! \n" + context.Channel.Mention + " is now closed for 2 mins to cool things down.\n\n*Remember to avoid offtopic chats!*");
-            await channel.AddOverwriteAsync(tempBan, DSharpPlus.Permissions.None, DSharpPlus.Permissions.SendMessages, "Temp timeout start");
+            await channel.AddOverwriteAsync(context.Guild.EveryoneRole, DSharpPlus.Permissions.None, DSharpPlus.Permissions.SendMessages, "Temp timeout start");
             
             var embedChat = new DiscordEmbedBuilder
             {
@@ -53,8 +53,7 @@ namespace Mobo
 
             await target.SendMessageAsync(embed: embedChat);
             await Task.Delay(120000);
-            await channel.AddOverwriteAsync(tempBan, DSharpPlus.Permissions.SendMessages, DSharpPlus.Permissions.None, "Temp timeout stop");
-
+            await channel.AddOverwriteAsync(context.Guild.EveryoneRole, DSharpPlus.Permissions.SendMessages, DSharpPlus.Permissions.None, "Temp timeout stop");
         }
 
         [Command("warn")]
